82f96a9ef8b7c3c9339bc1aaabd575655a280fea
Aleksandr Shirenkov <ashir84@yandex.ru>
2023-08-27 13:45:21 +0300

    feat: Вынести в файл констант настройки подключения

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
-const express = require('express');
-require('dotenv').config();
-const bodyParser = require('body-parser');
-const helmet = require('helmet');
+const express = require("express");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
 
 // Слушаем 3000 порт
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const { errors } = require('celebrate');
-const limiter = require('./utils/express-limiter');
+const { errors } = require("celebrate");
+const limiter = require("./utils/express-limiter");
 
-const route = require('./routes/index');
+const route = require("./routes/index");
 
-const handlerErrors = require('./middlewares/handlerErrors');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
-const cors = require('./middlewares/cors');
+const handlerErrors = require("./middlewares/handlerErrors");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
+const cors = require("./middlewares/cors");
 
-const { PORT = 3000 } = process.env;
+const { NODE_ENV, MONGODB_PROD_CON, SERVER_PROD_PORT } = process.env;
+const { MONGODB_DEV_CON, SERVER_DEV_PORT } = require("./utils/constants");
+
+const MONGODB_CON =
+  NODE_ENV === "production" ? MONGODB_PROD_CON : MONGODB_DEV_CON;
+const SERVER_PORT =
+  NODE_ENV === "production" ? SERVER_PROD_PORT : SERVER_DEV_PORT;
 
 // подключаемся к серверу mongo
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {});
+mongoose.connect(MONGODB_CON, {});
 
 const app = express();
 app.use(helmet());
@@ -34,7 +40,7 @@ app.use(errorLogger);
 app.use(errors());
 app.use(handlerErrors);
 
-app.listen(PORT, () => {
+app.listen(SERVER_PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
-  console.log(`App listening on port ${PORT}`);
+  console.log(`App listening on port ${SERVER_PORT}`);
 });
--- utils/constants.js
+++ utils/constants.js
@@ -1,8 +1,10 @@
-const statusCode = {
+module.exports.statusCode = {
   ok: 200,
   created: 201,
   badRequest: 400,
   notFound: 404,
   internalServerError: 500,
 };
-module.exports = { statusCode };
+
+module.exports.MONGODB_DEV_CON = "mongodb://127.0.0.1:27017/bitfilmsdb";
+module.exports.SERVER_DEV_PORT = 3000;
